e77cde04ab396519f40dfe986dfd5db0cd9b8c0b
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-29 11:39:14 +0800

    category and product models

--- src/main/java/com/eshopping/decemberprojectmodule/models/Products.java
+++ src/main/java/com/eshopping/decemberprojectmodule/models/Products.java
@@ -1,4 +1,20 @@
 package com.eshopping.decemberprojectmodule.models;
 
 public class Products {
+    private long id;
+    private String title;
+    private String description;
+    private double price;
+    private long categoryId;
+
+    public Products() {
+    }
+
+    public Products(long id, String title, String description, double price, long categoryId) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
 }
