316533d3dbf0916c6674038ba6731b296c694f60
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-29 11:19:09 +0800

    initial folders

---
+++ src/main/java/com/eshopping/decemberprojectmodule/DecemberProjectModuleApplication.java
@@ -0,0 +1,13 @@
+package com.eshopping.decemberprojectmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecemberProjectModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecemberProjectModuleApplication.class, args);
+    }
+
+}
