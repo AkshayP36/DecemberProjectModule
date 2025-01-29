7b86bff6f473412bc8b5207cf667027ee1a0805a
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-29 12:20:26 +0800

    controller files initialize

--- src/main/java/com/eshopping/decemberprojectmodule/controllers/CategoryController.java
+++ src/main/java/com/eshopping/decemberprojectmodule/controllers/CategoryController.java
@@ -1,4 +1,7 @@
 package com.eshopping.decemberprojectmodule.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoryController {
 }
--- src/main/java/com/eshopping/decemberprojectmodule/controllers/ProductsController.java
+++ src/main/java/com/eshopping/decemberprojectmodule/controllers/ProductsController.java
@@ -1,4 +1,26 @@
 package com.eshopping.decemberprojectmodule.controllers;
 
+import com.eshopping.decemberprojectmodule.models.Products;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductsController {
+
+    @RequestMapping(value = "/product/13", method = RequestMethod.POST)
+    public static void createProduct(Products product){
+
+    }
+    @GetMapping("/product")
+    public static Products getProductById(long productId){
+        return null;
+    }
+    public static void updateProduct(Products product){
+
+    }
+    public static void deleteProductById(long productId){
+
+    }
 }
