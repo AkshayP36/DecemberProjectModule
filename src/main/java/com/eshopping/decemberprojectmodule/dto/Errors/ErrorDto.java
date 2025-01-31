22137109b474d5b52a357ac8f6bdd0b37e659f3a
Akshay Patil <studyakshaystudy@outlook.com>
2025-01-31 11:54:38 +0800

    Exception handling

---
+++ src/main/java/com/eshopping/decemberprojectmodule/dto/Errors/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.eshopping.decemberprojectmodule.dto.Errors;
+
+public class ErrorDto {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/eshopping/decemberprojectmodule/exceptions/Products/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eshopping.decemberprojectmodule.exceptions.Products;
+
+public class ProductNotFoundException extends Exception{
+
+    public ProductNotFoundException(String message){
+        super(message);
+    }
+}
